69b62afb8ef8cc0c4b2859631455f079e0b8c3af
NaomiBda <naomi.berda@gmail.com>
2018-05-30 14:55:15 +0200

    Fichiers ajoutés une premiere fois

---
+++ README.txt
@@ -0,0 +1,1 @@
+Ceci présente juste le projet, c'est a dire apprendre à manier Git et Github.
---
+++ fichier1.py
@@ -0,0 +1,9 @@
+#fichier de maths
+def racine_carree(x):
+	return(x**(1/2))
+
+def carre(x):
+	return(x*2)
+
+print(racine_carree(4) , carre(4))
+
